bdf4c19b77307e4f035dc9f30e73f6c7a90c2d20
GitTianxun163 <tianxunwx@163.com>
2022-12-10 14:47:06 +0800

    savePojects

--- js/load.js
+++ js/load.js
@@ -1,11 +1,14 @@
-var xhr = new XMLHttpRequest();
-xhr.open('get', 'https://v1.jinrishici.com/all.txt');
-xhr.onreadystatechange = function () {
-if (xhr.readyState === 4) {
-    var gushici = document.getElementById('gushici');
-    gushici.innerHTML = xhr.responseText+" "+"<a href='/index.html' class='ff'>(换一句)</a>";
+var summonG = function(){
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', 'https://v1.jinrishici.com/all.txt');
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        var gushici = document.getElementById('gushici');
+        gushici.innerHTML = xhr.responseText+" "+"<a onclick='javascript:summonG()' class='ff'>(换一句)</a>";
+    }
+    };
+    xhr.send();
 }
-};
-xhr.send();
 
 document.getElementById("main").style.height = window.innerHeight;
+summonG()
